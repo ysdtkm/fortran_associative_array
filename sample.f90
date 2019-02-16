5d409c5e6208290ac1483fa01d0a031250495c90
Takuma Yoshida <35924685+ysdtkm@users.noreply.github.com>
2019-02-16 17:33:10 -0500

    modify

--- sample.f90
+++ sample.f90
@@ -3,17 +3,18 @@ program sample
   use dict_mod, only: dict, exists, get_size, get_val, insert_or_assign, remove
   implicit none
 
-  type(dict) :: ages
+  type(dict) :: ages  ! Initialized empty
 
   call insert_or_assign(ages, "Alice", 28)
-  call insert_or_assign(ages, "Bob", 13)
+  call insert_or_assign(ages, "Bob",   13)
   call insert_or_assign(ages, "Carol", 47)
+  call insert_or_assign(ages, "Alice", 35)  ! Updated
 
-  print *, "Alice is ", get_val(ages, "Alice"), "years old"
-  print *, "Is Dave included? ", exists(ages, "Dave")
+  print *, "Alice is", get_val(ages, "Alice"), "years old"
+  print *, "Do we know Dave's age?", exists(ages, "Dave")
 
   call remove(ages, "Bob")
 
-  print *, "Now the dictionary has", get_size(ages), "records"
+  print *, "Now we know the ages of", get_size(ages), "people"  ! Alice and Carol
 end program sample
 
