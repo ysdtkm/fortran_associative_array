7bb5c5bc4680035dbc115c94c2066f2b350d20b8
Takuma Yoshida <35924685+ysdtkm@users.noreply.github.com>
2019-02-16 11:19:35 -0500

    fix tests

--- test/add_erase_random/add_erase_random.f90
+++ test/add_erase_random/add_erase_random.f90
@@ -1,10 +1,10 @@
 #include <dtypes.h>
 program add_erase_random
-  use treap_mod, only: treap, add, find, remove, show, get_size, key_exists
+  use dict_mod, only: dict, insert_or_assign, get_val, remove, show, get_size, exists
   implicit none
 
   integer, parameter :: n = 10000
-  type(treap) :: t
+  type(dict) :: t
   integer :: i, a(n), seed(100)
   real(8) :: r
   valtype :: b
@@ -18,16 +18,16 @@ program add_erase_random
   end do
 
   do i = 1, n
-    call add(t, a(i), float(a(i)))
+    call insert_or_assign(t, a(i), float(a(i)))
   end do
 
   do i = 1, n
-    b = find(t, a(i))
+    b = get_val(t, a(i))
     if (abs(b - a(i)) > 0.0001) stop 2
   end do
 
   do i = 1, n
-    if (key_exists(t, a(i))) then
+    if (exists(t, a(i))) then
       call remove(t, a(i))
     end if
   end do
@@ -35,7 +35,7 @@ program add_erase_random
   if (get_size(t) /= 0) stop 4
 
   do i = 1, n
-    call add(t, a(i), float(a(i)))
+    call insert_or_assign(t, a(i), float(a(i)))
   end do
 end program add_erase_random
 
--- test/get_kth/get_kth.f90
+++ test/get_kth/get_kth.f90
@@ -1,18 +1,18 @@
 program get_kth
-  use treap_mod, only: treap, add, find, remove, show, get_size, get_kth_key
+  use dict_mod, only: dict, insert_or_assign, get_val, get_kth_key
   implicit none
 
   integer, parameter :: n = 10000
-  type(treap) :: t
+  type(dict) :: t
   integer :: i
   real(4), parameter :: eps = 1e-6
 
   do i = 1, n
-    call add(t, i, float(i))
+    call insert_or_assign(t, i, float(i))
   end do
 
   do i = 1, n
-    if (abs(find(t, get_kth_key(t, i)) - i) > eps) stop 2
+    if (abs(get_val(t, get_kth_key(t, i)) - i) > eps) stop 2
   end do
 end program get_kth
 
