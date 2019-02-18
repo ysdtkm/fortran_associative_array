7f4515f2ae5ed1e3f66f51fa378b0b8dc3777f47
Takuma Yoshida <35924685+ysdtkm@users.noreply.github.com>
2019-02-18 11:42:35 -0500

    AC

--- test/speedtest/speedtest.f90
+++ test/speedtest/speedtest.f90
@@ -1,4 +1,4 @@
-#define jmax 20
+#define jmax 18
 program speedtest
   use dict_mod, only: dict, insert_or_assign, get_val, remove, get_size, exists
   implicit none
@@ -7,43 +7,70 @@ program speedtest
     integer :: cnt = 0
     integer :: keys(2 ** jmax)
     real(4) :: vals(2 ** jmax)
-
   end type linear_set
 
-  type(dict) :: t
-  type(linear_set) :: ls
-  integer :: b, i, j, n, seed(100), t1, t2, t_rate
-  integer, allocatable :: a(:)
+  integer :: b, j, seed(100), t1, t2, t_rate
   real(4) :: r
+
+  seed(:) = 0
+  call random_seed(put=seed)
 
   do b = 0, 1
     do j = 5, jmax
-      if (b == 0 .and. j > 17) cycle
-      seed(:) = 0
-      call random_seed(put=seed)
-      n = 2 ** j
-      allocate(a(n))
-
-      do i = 1, n
-        call random_number(r)
-        a(i) = floor(r * n)
-      end do
-
-      call system_clock(t1)
-      do i = 1, n
-        if (b > 0) then
-          call insert_or_assign(t, a(i), float(a(i)))
-        else
-          call linear_insert_or_assign(ls, a(i), float(a(i)))
-        end if
-      end do
-      call system_clock(t2, t_rate)
-      print *, n, (t2 - t1) / dble(t_rate) / dble(n)
-      deallocate(a)
+      if (b == 0 .and. j > 16) cycle
+      if (b == 0) then
+        call test_linear(j)
+      else
+        call test_treap(j)
+      end if
     end do
   end do
 
   contains
+
+  subroutine test_treap(j)
+    implicit none
+    integer, intent(in) :: j
+    integer :: n, i
+    integer :: a(2 ** j)
+    type(dict) :: t
+    n = 2 ** j
+
+    do i = 1, n
+      call random_number(r)
+      a(i) = floor(r * n)
+    end do
+
+    call system_clock(t1)
+    do i = 1, n
+      call insert_or_assign(t, a(i), float(a(i)))
+    end do
+    call system_clock(t2, t_rate)
+
+    print *, n, (t2 - t1) / dble(t_rate) / dble(n)
+  end subroutine test_treap
+
+  subroutine test_linear(j)
+    implicit none
+    integer, intent(in) :: j
+    integer :: n, i
+    integer :: a(2 ** j)
+    type(linear_set) :: ls
+    n = 2 ** j
+
+    do i = 1, n
+      call random_number(r)
+      a(i) = floor(r * n)
+    end do
+
+    call system_clock(t1)
+    do i = 1, n
+      call linear_insert_or_assign(ls, a(i), float(a(i)))
+    end do
+    call system_clock(t2, t_rate)
+
+    print *, n, (t2 - t1) / dble(t_rate) / dble(n)
+  end subroutine test_linear
 
   subroutine linear_insert_or_assign(ls, key, val)
     implicit none
