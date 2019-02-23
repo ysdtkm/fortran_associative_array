9c50c3633024d9b4c99cd1d0dbf29ac836beb021
Takuma Yoshida <35924685+ysdtkm@users.noreply.github.com>
2019-02-23 08:17:27 -0500

    rehash

--- hash_table.f90
+++ hash_table.f90
@@ -5,6 +5,8 @@ program hash_table  ! ttk module
   implicit none
 
   integer(4), parameter :: min_alloc = 64
+  integer(4), parameter :: fnv_offset_basis_32 = -2128831035  ! 0x811c9dc5
+  integer(4), parameter :: fnv_prime_32 = 16777619
   enum, bind(c)
     enumerator :: unused, used, deleted
   end enum
@@ -12,11 +14,13 @@ program hash_table  ! ttk module
   type dictionary
     integer(4) :: num_elem = 0
     integer(4) :: num_del = 0
-    integer(4) :: alloc_size = 0
+    integer(4) :: alloc_size = 0  ! Power of 2 or 0
     integer(4), allocatable :: flags(:)
     keytype, allocatable :: keys(:)
     valtype, allocatable :: vals(:)
   end type dictionary
+
+  call test()
 
   contains
 
@@ -24,31 +28,115 @@ program hash_table  ! ttk module
     implicit none
     type(dictionary) :: di
     call insert_or_assign(di, 1, 1)
+    print *, di%flags
   end subroutine test
+
+  pure function fnv_1a_32_int(x)
+    implicit none
+    integer(4), intent(in) :: x
+    integer(4) :: fnv_1a_32_int, i
+    integer(1) :: c(4)
+    fnv_1a_32_int = fnv_offset_basis_32
+    c = transfer(x, c)
+    do i = 1, 4
+      fnv_1a_32_int = fnv_prime_32 * ieor(fnv_1a_32_int, int(c(i), kind=4))
+    end do
+  end function fnv_1a_32_int
+
+  function fnv_1a_32(s)
+    implicit none
+    character(*), intent(in) :: s
+    integer(4) :: fnv_1a_32, i
+    integer(1) :: c(len(s))
+    fnv_1a_32 = fnv_offset_basis_32
+    c = transfer(s, c, len(s))
+    do i = 1, len(s)
+      fnv_1a_32 = fnv_prime_32 * ieor(fnv_1a_32, int(c(i), kind=4))
+    end do
+  end function fnv_1a_32
 
   subroutine insert_or_assign(di, key, val)
     implicit none
     type(dictionary), intent(inout) :: di
     keytype, intent(in) :: key
     valtype, intent(in) :: val
+
     if ((di%num_elem + di%num_del) * 2 >= di%alloc_size) then
       call rehash(di)
     end if
-
-    ! allocate
-
+    call pure_insert(di, key, val)
   end subroutine insert_or_assign
+
+  subroutine pure_insert(di, key, val)
+    ! Dictionary is assumed to have enough space
+    implicit none
+    type(dictionary), intent(inout) :: di
+    keytype, intent(in) :: key
+    valtype, intent(in) :: val
+    integer(4) :: addr, i
+    logical :: overflow
+    addr = get_initial_addr(di%alloc_size, key)
+    overflow = .true.
+    do i = 1, di%alloc_size
+      if (di%flags(addr) == unused) then
+        di%flags(addr) = used
+        di%keys(addr) = key
+        di%vals(addr) = val
+        overflow = .false.
+        exit
+      else
+        addr = increment(di%alloc_size, addr)
+      end if
+    end do
+    if (overflow) stop 1
+  end subroutine pure_insert
+
+  pure function get_initial_addr(n, key)
+    implicit none
+    keytype, intent(in) :: key
+    integer(4), intent(in) :: n
+    integer(4) :: get_initial_addr
+    get_initial_addr = fnv_1a_32_int(key)
+    get_initial_addr = iand(get_initial_addr, n - 1) + 1  ! 1-based
+  end function get_initial_addr
+
+  pure function increment(n, addr)
+    implicit none
+    integer(4), intent(in) :: n, addr
+    integer(4) :: increment
+    increment = iand(addr, n - 1) + 1  ! 1-based
+  end function increment
 
   subroutine rehash(di)
     implicit none
     type(dictionary), intent(inout) :: di
-    if (di%alloc_size == 0) then
+    integer(4), allocatable :: old_flags(:)
+    keytype, allocatable :: old_keys(:)
+    valtype, allocatable :: old_vals(:)
+    integer(4) :: i, n
+    n = di%alloc_size
+    if (n == 0) then
       di%alloc_size = min_alloc
-      allocate(di%flags(di%alloc_size))
-      allocate(di%keys(di%alloc_size))
-      allocate(di%vals(di%alloc_size))
+      allocate(di%flags(di%alloc_size), &
+        &      di%keys(di%alloc_size), &
+        &      di%vals(di%alloc_size))
+      di%flags(:) = unused
     else
-      di%alloc_size = di%alloc_size * 2
+      allocate(old_flags(n), old_keys(n), old_vals(n))
+      old_flags = di%flags
+      old_keys = di%keys
+      old_vals = di%vals
+      di%alloc_size = 2 * n
+      allocate(di%flags(di%alloc_size), &
+        &      di%keys(di%alloc_size), &
+        &      di%vals(di%alloc_size))
+      di%flags(:) = unused
+      do i = 1, n
+        if (old_flags(i) == used) then
+          call pure_insert(di, old_keys(i), old_vals(i))
+        end if
+      end do
+      deallocate(old_flags, old_keys, old_vals)
     end if
   end subroutine rehash
 
