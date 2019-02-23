eafb1783fdb8ad6b82136111351f3aefac4f1ca0
Takuma Yoshida <35924685+ysdtkm@users.noreply.github.com>
2019-02-23 06:34:37 -0500

    modify

--- hash_table.f90
+++ hash_table.f90
@@ -4,20 +4,50 @@
 module hash_table
   implicit none
 
+  integer(4), parameter :: min_alloc = 64
+  enum, bind(c)
+    enumerator :: unused, used, deleted
+  end enum
+
   type dictionary
     integer(4) :: num_elem = 0
     integer(4) :: num_del = 0
     integer(4) :: alloc_size = 0
-    integer(4), allocatable :: flags
-    keytype, allocatable :: keys
-    valtype, allocatable :: vals
+    integer(4), allocatable :: flags(:)
+    keytype, allocatable :: keys(:)
+    valtype, allocatable :: vals(:)
   end type dictionary
 
   contains
 
+  subroutine insert_or_assign(di, key, val)
+    implicit none
+    type(dictionary), intent(inout) :: di
+    keytype, intent(in) :: key
+    valtype, intent(in) :: val
+    if ((di%num_elem + di%num_del) * 2 >= di%alloc_size) then
+      call rehash(di)
+    end if
+
+    ! allocate
+
+  end subroutine insert_or_assign
+
+  subroutine rehash(di)
+    implicit none
+    type(dictionary), intent(inout) :: di
+    if (di%alloc_size == 0) then
+      di%alloc_size = min_alloc
+      allocate(di%flags(di%alloc_size))
+      allocate(di%keys(di%alloc_size))
+      allocate(di%vals(di%alloc_size))
+    else
+      di%alloc_size = di%alloc_size * 2
+    end if
+  end subroutine rehash
+
   ! exists
   ! get_val
-  ! insert_or_assign
   ! delete
   ! get_size
   ! get_keys_vals
